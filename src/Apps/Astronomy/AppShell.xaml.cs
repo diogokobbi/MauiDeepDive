31f6b1385e39befcc3487ad76bd70f1506cef247
Kobbi <diogo.kobbi@choiceholding.com>
2022-09-22 21:28:50 -0300

    Módulo 6 - Create multi-page .NET MAUI apps with tab and flyout navigation

---
+++ src/Apps/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Astronomy;
+using Pages;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+    }
+}
